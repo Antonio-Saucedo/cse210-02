821f9b4726a5504330dda231d64708ff365ccdab
Antonio-Saucedo <antoniojesus@byui.edu>
2022-05-05 21:25:25 -0600

    results method commit

--- card_game/hilo/game/director.py
+++ card_game/hilo/game/director.py
@@ -1,48 +1,59 @@
 from game.card import Card
+from colorama import Fore
+from colorama import Style
+from colorama import init
+init(autoreset=True)
 
 class Director:
     # constructor function
     # this function initializes the attributes
     # attributes: 
     # points: integer - holds the score of the game
+    # play_again - boolean - tracks if user wants to play again or not
     # card1: an instance of the class card - holds the card
     # card2: another instance of the class card - holds another card
     def __init__(self):
+        self.play_again = True
         self.points = 300
         self.card1 = Card()
         self.card2 = Card()
         
 
 
-    # start game method
     # manuel
-    """Starts the game by running the main game loop.
+    def start_game(self):
+        """Starts the game by running the main game loop.
         
         Args:
             self (Director): an instance of Director.
         """
-    def start_game(self):
-        print(self.card1.getValue())
-        print(self.card2.getValue())
+        while self.play_again & (self.points > 0):
+            print(self.card1.getValue())
+            print(self.card2.getValue())
+            self.results()
 
-    # get inputs method 
     # Godwin 
-    """Ask the user if they want to roll.
+        """Get player input on high/low guess for the next card and update the player's total score based on input.
 
         Args:
             self (Director): An instance of Director.
         """
 
-    # do updates function
     # Antonio
-    """Updates the player's score.
+    def results(self):
+        """Displays the second card and the players score.
 
         Args:
             self (Director): An instance of Director.
         """
-    # do outputs function
+        if not self.play_again:
+            return
+
+        print(Fore.BLUE + Style.BRIGHT +f"The next card was: {self.card2.getValue()}")
+        print(Fore.GREEN + Style.BRIGHT + f"Your score is: {self.points}\n")
+
     # Cole
-    """Displays the dice and the score. Also asks the player if they want to roll again. 
+        """Ask the user if they want to play again. 
 
         Args:
             self (Director): An instance of Director.
