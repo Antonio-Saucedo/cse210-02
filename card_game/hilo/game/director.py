160db84b6886d438c469f6f087bce97c81826a78
Antonio-Saucedo <antoniojesus@byui.edu>
2022-05-06 08:07:26 -0600

    added/changed attributes, score system, debugged.

--- card_game/hilo/game/director.py
+++ card_game/hilo/game/director.py
@@ -1,22 +1,22 @@
 from game.card import Card
 from colorama import Fore
 from colorama import Style
-from colorama import init
-init(autoreset=True)
 
 class Director:
     # constructor function
     # this function initializes the attributes
     # attributes: 
     # points: integer - holds the score of the game
-    # play_again - boolean - tracks if user wants to play again or not
+    # guess - string - holds the player guess
+    # play - boolean - tracks if player wants to play again or not
     # card1: an instance of the class card - holds the card
     # card2: another instance of the class card - holds another card
     def __init__(self):
-        self.play_again = True
+        self.play = True
+        self.guess = ""
         self.points = 300
-        self.card1 = Card()
-        self.card2 = Card()
+        self.card1 = 0
+        self.card2 = 0
         
 
 
@@ -27,11 +27,13 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        while self.play_again & (self.points > 0):
+        while self.play & (self.points > 0):
+            self.card1 = Card()
+            self.card2 = Card()
             print(self.card1.getValue())
-            print(self.card2.getValue())
-            self.get_inputs
+            self.get_inputs()
             self.results()
+            self.play_again()
 
     # Godwin 
         """Get player input on high/low guess for the next card and update the player's total score based on input.
@@ -40,9 +42,11 @@ class Director:
             self (Director): An instance of Director.
         """
     def get_inputs(self):
-        higher_lower = input("Higher or Lower? [h/l] ")
-        self.play_again = (higher_lower == "h" or higher_lower == "l")
+        if not self.play:
+            return
 
+        higher_lower = input("Higher or Lower? [h/l] ")
+        self.guess = (higher_lower == "h" or higher_lower == "l")
 
     # Antonio
     def results(self):
@@ -51,11 +55,20 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        if not self.play_again:
+        if not self.play:
             return
 
         print(Fore.BLUE + Style.BRIGHT +f"The next card was: {self.card2.getValue()}")
-        print(Fore.GREEN + Style.BRIGHT + f"Your score is: {self.points}\n")
+
+        if ((self.card1.getValue() < self.card2.getValue()) & self.guess == "h") | ((self.card1.getValue() > self.card2.getValue()) & self.guess == "l"):
+            self.points -= 75
+        elif (self.card1.getValue() == self.card2.getValue()):
+            self.points = self.points
+        else:
+            self.points += 100
+
+        print(Fore.GREEN + Style.BRIGHT + f"Your score is: {self.points}\n" + Style.RESET_ALL)
+        print("test")
 
     # Cole
         """Ask the user if they want to play again. 
@@ -64,9 +77,9 @@ class Director:
             self (Director): An instance of Director.
         """
     def play_again(self):
-        if not self.play_again:
+        if not self.play:
             return
-        
+
         answer = ""
 
         while (answer != "y" and answer != "n"):
@@ -77,8 +90,8 @@ class Director:
         
         if answer == "n":
             print ("\nGame over. You chose to stop playing.")
-            return False
+            self.play = False
         else:
-            return True
+            self.play = True
 
     
